8fac185016569c89dbb5d6ef72bc6dc6f2c47494
unknown <fehandke@gmail.com>
2023-02-04 22:43:46 +0100

    Removing files

--- pathhandler.h
+++ pathhandler.h
@@ -8,6 +8,6 @@ class PathHandler
 	std::wstring m_filepath;
 public:
 
-	void setPath(std::wstring filepath);
-	std::wstring getPath();
+	void setPath(std::wstring filepath) { m_filepath = filepath; }
+	std::wstring getPath() { return m_filepath; }
 };
